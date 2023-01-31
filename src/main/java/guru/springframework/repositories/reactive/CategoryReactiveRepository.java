5322719a8e928aa83ec86d3aa11f06d262e8b2e2
Yakodelaragua <araycdd@gmail.com>
2023-01-31 15:42:43 +0100

    reactiuve

--- src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
@@ -4,8 +4,6 @@ import guru.springframework.domain.Category;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
-
 public interface CategoryReactiveRepository extends ReactiveMongoRepository<Category, String> {
     Mono<Category> findByDescription(String description);
 
